7391d637865ee008b9653a6d94f3ae16cd8934c1
nicole chung <nicole.chung@gmail.com>
2019-03-10 18:31:12 -0400

    removing something in styled components that was causing @types/react-native to be installed

--- src/app.js
+++ src/app.js
@@ -17,6 +17,44 @@ export const colourOptions = [
   { value: 'silver', label: 'Silver', color: '#666666' },
 ]
 
+const dot = (color = '#ccc') => ({
+  alignItems: 'center',
+  display: 'flex',
+
+  ':before': {
+    backgroundColor: color,
+    borderRadius: 10,
+    content: '" "',
+    display: 'block',
+    marginRight: 8,
+    height: 10,
+    width: 10,
+  },
+})
+
+const colourStyles = {
+  control: styles => ({ ...styles, backgroundColor: 'white' }),
+  option: (styles, { data, isDisabled, isFocused, isSelected }) => {
+    const color = chroma(data.color);
+    return {
+      ...styles,
+      backgroundColor: isDisabled
+        ? null
+        : isSelected ? data.color : isFocused ? color.alpha(0.1).css() : null,
+      color: isDisabled
+        ? '#ccc'
+        : isSelected
+          ? chroma.contrast(color, 'white') > 2 ? 'white' : 'black'
+          : data.color,
+      cursor: isDisabled ? 'not-allowed' : 'default',
+    };
+  },
+  input: styles => ({ ...styles, ...dot() }),
+  placeholder: styles => ({ ...styles, ...dot() }),
+  singleValue: (styles, { data }) => ({ ...styles, ...dot(data.color) }),
+}
+
+
 const App = ({ store }) => {
   const [todo, setTodo] = useState('')
   return (
@@ -25,6 +63,12 @@ const App = ({ store }) => {
         <div>
           <form>
             <input value={todo} onChange={e => setTodo(e.target.value)} />
+            <Select
+              defaultValue={colourOptions[2]}
+              label="Single select"
+              options={colourOptions}
+              styles={colourStyles}
+            />
             <button type='submit'>Add</button>
           </form>
         </div>
